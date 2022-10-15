dfdf3a407f14ecd8ad7e19c137089f5345aa7608
Oksana Karpova <oksanachv@gmail.com>
2022-10-15 14:28:55 +0200

    Practice create objects

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,30 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const book = {
+  name: "Child of the North",
+  author: "Edith Pattou",
+  translator: "Tetiana Jevlojeva",
+  cover: "hard",
+  pagesNum: 416,
+};
+
+console.log("Object:", book);
+console.log("Num of pages:", book.pagesNum);
+
+const pot = {
+  size: "4L",
+  material: "stainell steel",
+  hasLid: true,
+  dish: {
+    name: "soup",
+    color: "multicolor",
+    mainIngredient: "vegetables",
+    level: "easy",
+  },
+};
+
+console.log("Object:", pot);
+console.log("Has a lid:", pot.hasLid);
+console.log("A dish inside:", pot.dish.name);
