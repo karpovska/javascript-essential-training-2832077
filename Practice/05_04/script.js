b82729ae1942b8a5df63e8fba14c4f5401c80e20
Oksana Karpova <oksanachv@gmail.com>
2023-01-09 14:04:21 +0100

    Tasks for querySelector methods

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,15 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+
+let lidStatus = document.querySelector(".backpack__lid span").innerHTML.trim();
+console.log(lidStatus);
+
+let openButton = document.querySelector(".lid-toggle");
+console.log(openButton);
+
+// let openBackpack = () => {
+//   if (openButton.innerHTML === "closed") {
+//     lidStatus.innerHTML = "open";
+//   }
+// };
