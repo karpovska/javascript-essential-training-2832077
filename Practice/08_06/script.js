3ca157c81cd3bc5d87c298db52e76dc43ef551f2
Oksana Karpova <oksanachv@gmail.com>
2023-02-15 16:59:54 +0100

    Creating function with different methods

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,20 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+
+function addClass() {
+  document.querySelector("main").classList.add("container");
+  console.log(document.querySelector("main").classList);
+}
+addClass();
+
+const addAnotherClass = function () {
+  document.querySelector("main").classList.add("left-box");
+  console.log(document.querySelector("main").classList);
+};
+addAnotherClass();
+
+(function () {
+  document.querySelector("main").classList.add("main");
+  console.log(document.querySelector("main").classList);
+})();
