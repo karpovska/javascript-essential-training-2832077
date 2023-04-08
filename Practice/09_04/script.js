8599d2f4d6e64e49bfa5e40fb166ded69b404f64
Oksana Karpova <oksanachv@gmail.com>
2023-04-08 14:01:44 +0200

    Added event listeners on mouse click and key press

--- Practice/09_04/script.js
+++ Practice/09_04/script.js
@@ -15,3 +15,36 @@ gridContainer.addEventListener("mouseenter", () => {
 gridContainer.addEventListener("mouseleave", () => {
   gridContainer.style.outline = "";
 });
+
+//select all grid cells
+const gridCells = document.querySelectorAll(".cell");
+
+//generate random hex color
+const randomColor = () => {
+  let hexColor = Math.floor(Math.random() * 16777215).toString(16);
+  return hexColor;
+};
+
+gridCells.forEach((cell) => {
+  cell.addEventListener("mouseenter", () => {
+    cell.style.outline = "4px solid #D0D050";
+  });
+
+  cell.addEventListener("mouseleave", () => {
+    cell.style.outline = "";
+  });
+
+  cell.addEventListener("mousedown", () => {
+    cell.style.backgroundColor = `#${randomColor()}`;
+  });
+});
+
+//set a background theme when a specific key pressed
+const body = document.querySelector("body");
+body.addEventListener("keydown", (event) => {
+  if (event.code === "KeyQ") {
+    body.style.backgroundColor === ""
+      ? (body.style.backgroundColor = "hsl(201, 34%, 13%)")
+      : (body.style.backgroundColor = "");
+  }
+});
